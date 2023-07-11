93fece9bf5954231f9a8260f7fe96c1d21fa02ad
Theo McMullien <theomcmullien.16@gmail.com>
2023-07-11 15:05:07 +1000

    added fixed background behind scrolling background

--- src/App.js
+++ src/App.js
@@ -16,21 +16,23 @@ function App() {
         <Element id='section1' name="home" className="section">
           <Home />
         </Element>
-        <Element id='section2' name="about" className="section">
-          <About />
-        </Element>
-        <Element id='section3' name="skills" className="section">
-          <Skills />
-        </Element>
-        <Element id='section4' name="education" className="section">
-          <Education />
-        </Element>
-        <Element id='section5' name="experience" className="section">
-          <Experience />
-        </Element>
-        <Element id='section6' name="contact" className="section">
-          <Contact />
-        </Element>
+        <div className='sub-content-container'>
+          <Element id='section2' name="about" className="section">
+            <About />
+          </Element>
+          <Element id='section3' name="skills" className="section">
+            <Skills />
+          </Element>
+          <Element id='section4' name="education" className="section">
+            <Education />
+          </Element>
+          <Element id='section5' name="experience" className="section">
+            <Experience />
+          </Element>
+          <Element id='section6' name="contact" className="section">
+            <Contact />
+          </Element>
+        </div>
       </div>
     </div>
   );
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,11 +8,11 @@ const About = () => {
         const windowHeight = window.innerHeight;
       
         if (scrollPosition > sectionPosition - windowHeight + 150 && scrollPosition < sectionPosition + 150) {
-          section.classList.add('about-animated');
+            section.classList.add('about-animated');
         } else {
             section.classList.remove('about-animated');
         }
-      });
+    });
     return (
         <div className='about-container'>
             <h1 className='about-section'>About</h1>
