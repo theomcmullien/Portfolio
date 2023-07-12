2f1c49f2d4d603ade52065aae4cd5332fb3e97aa
Theo McMullien <theomcmullien.16@gmail.com>
2023-07-13 01:37:08 +1000

    added hover effect to home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,12 @@ import TypeWriter from '../../components/typewriter/TypeWriter';
 const Home = () => {
     return (
         <div className='home-bg-container'>
-            <div className='home-container slide-left'>
-                <h2 className='mb-3'>Hello</h2>
-                <h1 className='mb-3'>I'm Theo McMullien</h1>
-                <TypeWriter />
+            <div className='home-container'>
+                <div className='home-group slide-left'>
+                    <h2 className='hover-grow mb-3'>Hello</h2>
+                    <h1 className='hover-grow mb-3'>I'm Theo McMullien</h1>
+                    <TypeWriter className='hover-grow' />
+                </div>
             </div>
         </div>
     );
