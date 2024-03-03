b26dfa940dc3365aa5c6329a346a50fcc9e2c8ac
Theo McMullien <theomcmullien.16@gmail.com>
2024-03-03 23:29:28 +1100

    removed italics from vs theme

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -4,6 +4,12 @@ const Skills = () => {
     return (
         <div className="skills-container">
             <h1>Skills</h1>
+            <div class="outer-box">
+                <div class="inner-box"></div>
+                <div class="text-wrapper">
+                    <p>Hover me</p>
+                </div>
+            </div>
         </div>
     );
 }
